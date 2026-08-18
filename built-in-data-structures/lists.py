bfe8f786161dcd3f7db8310cf305738bd6f19995
randhir-kr <230424755+randhir-kr@users.noreply.github.com>
2026-08-18 05:30:46 +0530

    added list example

---
+++ built-in-data-structures/lists.py
@@ -0,0 +1,11 @@
+# Lists[] : ordered, mutable, allows duplicate values
+
+shopping_cart = ["apple", "milk", "banana"]
+
+print("shopping cart before modifying: ", shopping_cart)
+
+# modifying an item
+
+shopping_cart[2] = "pineapple"
+
+print("shopping cart after modifying: ", shopping_cart)
